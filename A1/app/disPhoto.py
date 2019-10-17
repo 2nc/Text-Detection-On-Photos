9cced84e6eb7c296783a5c57e61f1f14019acba8
2nc <wnchit@163.com>
2019-10-17 11:13:53 +0800

    101622

--- A1/app/disPhoto.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, url_for
-from app import webapp
-
-
-@webapp.route('/disPhoto')
-def disPhoto():
-    return render_template("disPhoto.html")
--- A1/run.py
+++
@@ -1,3 +0,0 @@
-#!venv/bin/python
-from app import webapp
-webapp.run(debug=True)
