200cbc190e1ed9eaaa2ea126c5a532952920ef72
2nc <44913062+2nc@users.noreply.github.com>
2019-10-16 22:59:05 -0400

    101622

---
+++ A1/app/config.py
@@ -0,0 +1,9 @@
+db_config = {'user': 'root',
+             'password': '1234',
+             'host': '127.0.0.1',
+             'database': 'mydb'}
+
+conn_args = {
+
+    'region_name': 'us-east-1'
+}
